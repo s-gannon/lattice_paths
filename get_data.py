d94e119cbef95907e1afb7e2b98281c5726731e3
Eric Yager <ejyager00@gmail.com>
2021-10-02 19:42:12 -0500

    update file naming to account for potential command line arguments

--- get_data.py
+++ get_data.py
@@ -4,5 +4,9 @@ import lattice_paths as lp
 if __name__=="__main__":
     m = int(argv[1])
     n = int(argv[2])
+    if len(argv>3):
+        filename = argv[3]
+    else:
+        filename = f"lattice{m}by{n}data.tsv"
     results = lp.greedy_max_comparison(m,n)
-    results.to_csv(f"lattice{m}by{n}data.tsv", sep='\t', index=False)
+    results.to_csv(filename, sep='\t', index=False)
