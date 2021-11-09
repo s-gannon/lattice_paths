08fb0ec3f506ab6bf8d5578e9bb2741910d4a98f
Eric Yager <ejyager00@gmail.com>
2021-11-09 10:00:19 -0600

    update to convert to python dataframe better

--- python/convert_go_data.py
+++ python/convert_go_data.py
@@ -3,10 +3,16 @@ import pandas as pd
 
 def go_output_to_df(filename):
     df = pd.read_csv(filename,sep="\t", header=None,names=["m","n","k","greedy_order","max_order","greedy_is_max","greedy_set","max_sets"])
-    df = df.sort_values("k").reset_index()
+    df = df.sort_values("k")
+    df.reset_index(inplace=True)
+    df.drop(columns=["index"],inplace=True)
     df.greedy_set = [x[1:-1].split(", ") for x in df.greedy_set]
-    df.max_sets = [x.split(", ") for y in df.max_sets for x in y[2:-2].split("], [")]
+    df.max_sets = [[x.split(", ") for x in y[2:-2].split("], [")] for y in df.max_sets]
     return df
 
+def order_go_output(filename):
+    results = go_output_to_df(filename)
+    results.to_csv(filename[:-4]+"new.tsv", sep='\t', index=False)
+
 if __name__=="__main__":
-    print(go_output_to_df(argv[1]))
+    order_go_output(argv[1])
