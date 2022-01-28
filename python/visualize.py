0600e8c12af00dfc2979bb4ee0247be502fb7c39
Eric Yager <ejyager00@gmail.com>
2022-01-27 20:33:22 -0600

    add new visualization type

--- python/visualize.py
+++ python/visualize.py
@@ -129,7 +129,7 @@ def two_path_sets_svg(set1, set2, colors=('green','blue','orange')):
     return plot_paths(paths)
 
 def create_report(data_file, save_file):
-    data = pd.read_csv('4by3data.tsv',sep='\t')
+    data = pd.read_csv(data_file,sep='\t')
     data['greedy_set'] = [ast.literal_eval(x) for x in data['greedy_set']]
     data['max_sets'] = [ast.literal_eval(x) for x in data['max_sets']]
     title = f"<h1>Data for <i>k</i>-distinct paths on the {data.m[0]} by {data.n[0]} lattice</h1>"
@@ -156,7 +156,10 @@ def create_report(data_file, save_file):
             data_html.append("<p>In this case, the greedy algorithm does not give a maximum cardinality set.</p>")
             data_html.append(f"<p>The greedy set is visualized below. The paths are {row['greedy_set']}.</p>")
             data_html.append(plot_paths({'red': row['greedy_set']}))
-            data_html.append("<p>Below, we compare the greedy set to maximum sets. Shared paths are green, paths from only the greedy set are blue, and paths from a maximum set are orange.</p>")
+            data_html.append("<p>Below, we compare the greedy set to each maximum sets, with similar paths paired together and differences in bold.</p>")
+            for set_ in row['max_sets']:
+                data_html.append(highlight_diffs(match(row['greedy_set'],set_))+"<br> <br>")
+            data_html.append("<p>Below, we compare the greedy set to maximum sets on lattices. Shared paths are green, paths from only the greedy set are blue, and paths from a maximum set are orange.</p>")
             for set_ in row['max_sets']:
                 data_html.append(f"<h4>{set_}</h4>")
                 data_html.append(f"{two_path_sets_svg(row['greedy_set'],set_)}")
@@ -221,7 +224,7 @@ def match(set1, set2):
                     distances[key].pop(pairs[path], None)
                 distances.pop(path, None)
         if not updates:
-            for path in distances:
+            for path in tuple(distances.keys()):
                 if list(distances[path].values()).count(closest) > 1:
                     for p in distances[path]:
                         if distances[path][p]==closest:
@@ -230,9 +233,30 @@ def match(set1, set2):
                     for key in distances:
                         distances[key].pop(pairs[path], None)
                     distances.pop(path, None)
-        comparison = ""
-        for p1 in pairs:
-            comparison += set1[p1] + ' ' + set2[pairs[p1]] + "\n"
-        missing = int((len(set2)*(len(set2)-1))/2 - sum(pairs.values()))
-        comparison += ' '*(len(set1[0])+1) + set2[missing]
+    comparison = ""
+    for p1 in pairs:
+        comparison += set1[p1] + ' ' + set2[pairs[p1]] + "\n"
+    missing = int((len(set2)*(len(set2)-1))/2 - sum(pairs.values()))
+    comparison += ' '*(len(set1[0])+1) + set2[missing]
     return comparison
+
+def highlight_diffs(comparison):
+    pairs = comparison.split('\n')
+    new_string = '<table>'
+    for pair in pairs:
+        if pair[0]==' ':
+            new_string += "<tr><td></td><td>" + pair.split(' ')[-1] + "</td></tr>"
+        elif pair.split(' ')[0] == pair.split(' ')[1]:
+            new_string += "<tr><td>" + pair.split(' ')[0] + "</td><td>" + pair.split(' ')[1] + "</td></tr>"
+        else:
+            paths = pair.split(' ')
+            new_paths = ['','']
+            for i, c in enumerate(paths[0]):
+                if c!=paths[1][i]:
+                    new_paths[0]+="<b>"+c+"</b>"
+                    new_paths[1]+="<b>"+paths[1][i]+"</b>"
+                else:
+                    new_paths[0]+=c
+                    new_paths[1]+=paths[1][i]
+            new_string += "<tr><td>" + new_paths[0] + "</td><td>" + new_paths[1] + "</td></tr>"
+    return new_string + '</table>'
