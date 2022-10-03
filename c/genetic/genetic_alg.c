95e16fadaa8d40ea856daa86953da4f2f9290b6b
Spencer Gannon <spencer.gannon@valpo.edu>
2022-10-03 09:11:33 -0500

    updated ORCID citation

--- c/genetic/genetic_alg.c
+++ c/genetic/genetic_alg.c
@@ -1,4 +1,5 @@
 #include <math.h>
+#include <time.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
@@ -67,8 +68,9 @@ void path_print_converted(path path){
 	printf("\n");
 }
 //	A path is k-distinct from another if it shares fewer than k edges, or
-//	k shared edges < k, then theye are k-distinct from one another
-bool path_k_dist(path path_1, path path_2, uint16_t k){
+//	k shared edges < k, then theye are k-distinct from one another. E;se, it
+//	is k-equivalent to another path
+bool path_k_equiv(path path_1, path path_2, uint16_t k){
 	assert(path_1.m == path_2.m);
 	assert(path_1.n == path_2.n);
 	assert(k > 0);
@@ -78,10 +80,10 @@ bool path_k_dist(path path_1, path path_2, uint16_t k){
 	for(int i = 0; i < path_1.m + path.n; i++)
 		if(move_equiv(path1.moves[i], path2.moves[i]))
 			k_shared++;
-	if(k_shared < k)
+	if(k_shared < k)	//if num of edges shared is less than k, its k-dist
 		return false;
 	else
-		return true;
+		return true;	//if num of edges is <= k, its k-equiv
 }
 
 typedef struct {
@@ -111,13 +113,13 @@ float person_fitness(person* person, int ){
 	for(int i = 0; i < person->num_paths; i++){
 		for(int j = i + 1; j < person->num_paths; j++){
 			for(int k = 0; k < person->m + person->n; i++){
-				if(!path_k_dist(person->paths[i], person->paths[j]))
+				if(path_k_equiv(person->paths[i], person->paths[j]))
 					penalty++;
 			}
 		}
 	}
-	if(!penalty)	//will evaluate to true if penalty is 0
-		return INFINITY;
+	if(!penalty)	//if there are no k-equivalent paths, they're all k-dist
+		return INFINITY;	//set fitness to infinity
 	else
 		return (1.0/penalty);
 }
@@ -131,6 +133,39 @@ void person_show(person person){
 }
 
 int main(int argc, char** argv){
-	
+	//check your argc and argv before running
+	if(argc == 2 && !strncmp(argv[1], "-h", 2)){
+		printf("Usage: ./%s m n k g s\n", argv[0]);
+		printf("m, n : the width and height of the lattice\n");
+		printf("   k : the k-distinctness we're looking for\n");
+		printf("   g : the number of generations to run\n");
+		printf("   s : the size of the initial population (I think?)\n");
+		printf("\nRun ./%s -h to get this menu\n", argv[0]);
+		return EXIT_SUCCESS;
+	}
+	else if(argc != 6){
+		printf("Invalid number of arguments. Use ./%s m n k g s", argv[0]);
+		return EXIT_FAILURE;
+	}
+	//assert positive values for each
+	for(int i = 1; i < argc; i++)
+		assert(argv[i] > 0);
+	//seed srandom
+	srandom(time(NULL));
+	//start declaring and initializing vars
+	//set our argvs to actual variables
+	int lattice_size[2] = {(int)argv[1], (int)argv[2]};
+	int k_dist_val = (int)argv[3];
+	int num_generations = (int)argv[4];
+	int pop_size = (int)argv[5];
+	//create initial population
+	person* current_pop = (person*)calloc(pop_size, sizeof(person));
+	person* next_pop;
+	//randomized variables
+	int num_paths = (random() % 45) + 5;	//l in the paper
+	int duel_coeff = random() % pop_size;	//delta_current
+	int parent_coeff = (random() % duel_coeff)/2;	//alpha in the paper
+	int mutation_coeff = ranom() % num_paths;		//mu in the paper
+
 	return EXIT_SUCCESS;
 }
