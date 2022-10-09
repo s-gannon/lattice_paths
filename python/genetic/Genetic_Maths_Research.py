11a5b1d3356509f1231a9778ba06f83b481ef0f2
arielfayol37 <115123676+arielfayol37@users.noreply.github.com>
2022-10-09 01:53:18 -0500

    Update Genetic_Maths_Research.py

--- python/genetic/Genetic_Maths_Research.py
+++ python/genetic/Genetic_Maths_Research.py
@@ -2,63 +2,59 @@
 import random, winsound, numpy
 from datetime import datetime
 random.seed(getattr(datetime.now(), "microsecond"))
-class Term():
-    def __init__(self, bit_1 = 0, bit_2=0, bit_3 = 0):
-        self.bits = [bit_1, bit_2, random.randint(0,1)]
+            
+#generate a sequence depends on the values of m and n
+
 class Sequence():
     def __init__(self, m, n):
         assert m >= n
-        self.terms = []
-        for i in range(m+n):
-            self.terms.append(Term())
-            if i > 0:
-                if self.terms[i-1].bits[2] == 0 and self.terms[i-1].bits[0] < m:
-                    self.terms[i].bits[1] = self.terms[i-1].bits[1]
-                    self.terms[i].bits[0] = self.terms[i-1].bits[0] + 1
-                elif self.terms[i-1].bits[2]==0 and self.terms[i-1].bits[0] == m:
-                    self.terms[i-1].bits[2] = 1
-                    self.terms[i].bits[2] =1
-                    self.terms[i].bits[1] = self.terms[i-1].bits[1] + 1
-                    self.terms[i].bits[0] = self.terms[i-1].bits[0]
-                elif self.terms[i-1].bits[2]== 1 and self.terms[i-1].bits[1] < n:
-                    self.terms[i].bits[0] = self.terms[i-1].bits[0]
-                    self.terms[i].bits[1] = self.terms[i-1].bits[1] + 1
-                elif self.terms[i-1].bits[2] == 1 and self.terms[i-1].bits[1] == n:
-                    self.terms[i-1].bits[2] = 0
-                    self.terms[i].bits[2] = 0
-                    self.terms[i].bits[0] = self.terms[i-1].bits[0] + 1
-                    self.terms[i].bits[1] = self.terms[i-1].bits[1]
-            if i == (m+n)-1:
-                if m==n:
-                    if self.terms[i].bits[1] > self.terms[i].bits[0]:
-                        self.terms[i].bits[2] = 0
-                    else:
-                        self.terms[i].bits[2] = 1
-                elif self.terms[i].bits[1] == n:
-                    self.terms[i].bits[2] = 0
-                elif self.terms[i].bits[0] == m:
-                    self.terms[i].bits[2] = 1
+        r=random.randint(0,1)
+        self.terms = [[0,0,r]]
+        m_count = 0
+        n_count = 0
 
+        for i in range(m+n-1):
+            if r==1 and n_count<n:
                 
-            
-                        
+                n_count += 1
+                r2 = random.randint(0,1)
+                self.terms.append([self.terms[i][0],self.terms[i][1]+1, r2])
+                r= r2
+            elif r==0 and m_count<m:
+                m_count +=1
+                r2 = random.randint(0,1)
+                self.terms.append([self.terms[i][0]+1,self.terms[i][1], r2])
+                r= r2
+            elif r==1 and n_count==n:
+                self.terms[i] = [self.terms[i][0],self.terms[i][1], 0]
+                for j in range(i, m+n-1):
+                    
+                    self.terms.append([self.terms[j][0]+1,self.terms[j][1], 0])
+                break
+            elif r==0 and m_count==m:
+                self.terms[i] = [self.terms[i][0],self.terms[i][1], 1]
+                for j in range(i, m+n-1):
+                    
+                    self.terms.append([self.terms[j][0],self.terms[j][1]+1, 1])
+                break
+        if self.terms[-1][0] == m:
+            self.terms[-1][2] = 1
+        if self.terms[-1][1] == n:
+            self.terms[-1][2] = 0
     def show(self):
         for term in self.terms:
-            print(term.bits[0], term.bits[1], term.bits[2], sep=" ", end="   ")
+            print(term[0],term[1], term[2], sep=" ", end="   ")
     def compare(self,sequence,k):
         check= 0
         for i in range(len(sequence.terms)):
-            if sequence.terms[i].bits == self.terms[i].bits:#can make tis better someow
+            if sequence.terms[i] == self.terms[i]:#can make tis better someow
                 check += 1
         if check >= k:
             return 0
         else:
             return 1
-             
-#generate a sequence depends on the values of m and n
 
-    
-
+            
 class Genome():
     def __init__(self, num_sequences, m, n, k):
         self.sequences = []
@@ -77,7 +73,7 @@ class Genome():
             for j in range(i+1,len(self.sequences)):
                 #if i is not j:
                 for z in range(self.m + self.n):
-                    if self.sequences[i].terms[z].bits == self.sequences[j].terms[z].bits:
+                    if self.sequences[i].terms[z] == self.sequences[j].terms[z]:
                         check += 1
                 if check >= self.k:
                     penalty += 1
@@ -98,7 +94,7 @@ class Genome():
         #if random.randint(0, 37) % random.randint(2,3) == 0:
             #r = random.randint( 0, len(self.sequences) -1)
         if self.fitness()[-1]:
-            r = self.fitness()[-1][0][0]
+            r = self.fitness()[-1][random.randint(0,len(self.fitness()[-1])-1)][random.randint(0,1)]
             self.sequences[r] = Sequence(self.m, self.n)
         return self
     
@@ -121,19 +117,27 @@ class Population():
         self.r_fitnesses = numpy.array([])
         self.num_genes = 1
         self.sorted = False
-        self.max_size = 1000
+        self.max_size = int(size/2)
         self.roulette_ready = False
+        self.best_fitness = 0
+        self.bfi = 0
+        self.just_initialized = False
 
     def initialize(self):
         print("refilling")
         for z  in range(self.size):
             new_genome = Genome(self.num_genes,self.m,self.n,self.k)
             self.individuals.append(new_genome)
-            self.fitnesses = numpy.append(self.fitnesses,new_genome.fitness()[0])#you might later want to know where equivalences occur though 
-            if new_genome.fitness()[0] == 9999:
+            f = new_genome.fitness()[0]
+            self.fitnesses = numpy.append(self.fitnesses,f)#you might later want to know where equivalences occur though
+            if f > self.best_fitness:
+                self.best_fitness = f
+                self.bfi = z
+            if f == 9999:
                 new_genome.show()
                 winsound.PlaySound("SystemExclamation", winsound.SND_ALIAS)
                 return (len(self.individuals),self.c+1)
+        self.just_initialized = True        
     def bsort(self):
         #assert self.sorted == False
         assert len(self.individuals) == len(self.fitnesses)
@@ -152,17 +156,19 @@ class Population():
             self.sorted = True
         
     def battle(self, mode = "keep_fit"):
+        print("battling")
+        info_pool = 0.1
         if mode == "non_bias_random":
         #num_duels = int(duel_coef * len(population))
             l = len(self.individuals) # we cannot use this for indexes because the size of the population keeps changing, maybe copying to mating pool will be better
-            num_duels = random.randint(0, int(l/2))
+            #num_duels = random.randint(0, int(l/2))
             #for i in range(num_duels):
             while(len(self.individuals) > self.max_size):
-                index_1 = random.randint(0, len(self.individuals)-1) #we subtract 1 because the randint() function includes the bounds
+                index_1 = random.randint(0, len(self.individuals)-1) #we subtract 1 bescause the randint() function includes the bounds
                 index_2 = random.randint(0, len(self.individuals)-1)
                 while index_1 == index_2:
                     index_2 = random.randint(0, len(self.individuals)-1)
-                if self.individuals[index_1].fitness()[0] < self.individuals[index_2].fitness()[0]:
+                if self.fitnesses[index_1] < self.fitnesses[index_2]:
                     self.fitnesses = numpy.delete(self.fitnesses, index_1)
                     self.individuals.pop(index_1)
                 #elif population[index_1].fitness()[0] > population[index_2].fitness()[0]:
@@ -170,23 +176,24 @@ class Population():
                     self.fitnesses = numpy.delete(self.fitnesses, index_2)
                     self.individuals.pop(index_2)
 
+
         elif mode == "keep_fit":
             top_n = 10 
-            ##self.bsort()
-            l = len(self.individuals)
+            self.bsort()
+            #l = len(self.individuals)
             #num_duels = random.randint(0, int(l/2))
-            num_duels = 0.57
+            #num_duels = 0.57
             #for i in range(int(num_duels*l)):
-            while(len(self.individuals) > self.max_size):
+            while(len(self.individuals) > self.max_size):#not good! might get stuck in here due to "infor pool"
                 index_1 = random.randint(0, len(self.individuals)-1) #we subtract 1 because the randint() function includes the bounds
                 index_2 = random.randint(0, len(self.individuals)-1)
                 while index_1 == index_2:
                     index_2 = random.randint(0, len(self.individuals)-1)
-                if self.individuals[index_1].fitness()[0] < self.individuals[index_2].fitness()[0] and index_1 > top_n and index_1 < (len(self.individuals)-int(0.3*len(self.individuals))):
+                if self.fitnesses[index_1] < self.fitnesses[index_2] and index_1 > top_n and index_1 < (len(self.individuals)-int(info_pool*len(self.individuals))):
                     self.fitnesses = numpy.delete(self.fitnesses, index_1)
                     self.individuals.pop(index_1)
                 #elif population[index_1].fitness()[0] > population[index_2].fitness()[0]:
-                elif index_2 > top_n and index_2 < (len(self.individuals)-int(0.3*len(self.individuals))):
+                elif self.fitnesses[index_1] > self.fitnesses[index_2] and index_2 > top_n and index_2 < (len(self.individuals)-int(info_pool*len(self.individuals))):
                     self.fitnesses = numpy.delete(self.fitnesses, index_2)
                     self.individuals.pop(index_2)
     def parent_pick(self, mode = "roulette"):
@@ -195,8 +202,8 @@ class Population():
         if mode == "roulette":
             if not self.roulette_ready:
                 #increasin fitness of information pool
-                for fitness in self.fitnesses[-int(0.3*len(self.individuals)):]:
-                    fitness = 0.1
+                #for fitness in self.fitnesses[-int(0.3*len(self.individuals)):]:
+                    #fitness = 0.1
                                               
                 
                     #if not self.sorted:
@@ -209,7 +216,8 @@ class Population():
                     for i in range(len(self.r_fitnesses)):
                         self.r_fitnesses[i] = sumf + self.r_fitnesses[i]
                         sumf = self.r_fitnesses[i]
-                self.roulette_ready = True                 
+                self.roulette_ready = True
+                 
             rand1 = numpy.random.random(1)
             rand2 = numpy.random.random(1)
             parent_1_index = 0
@@ -237,7 +245,8 @@ class Population():
     def mating(self, mode = "random_random"):
         #mating_coef = ra'ndom.random()
         print("mating")
-        mating_coef = 0.555
+        mating_coef = 0.6
+        self.just_initialized = False
         l = len(self.individuals)
         for j in range(int(mating_coef * l)):
             co_coef = random.random()
@@ -273,8 +282,15 @@ class Population():
                 new_child_2= new_child_2.mutate()
                 self.children.append(new_child_1)
                 self.children.append(new_child_2)
+
                 a =new_child_1.fitness()[0]
                 b=new_child_2.fitness()[0]
+                if a > self.best_fitness:
+                    self.best_fitness = a
+                    self.bfi = len(self.individuals) + len(self.children) -2#yes, minus 2 
+                elif b > self.best_fitness:
+                    self.bset_fitness = b
+                    self.bfi = len(self.individuals) + len(self.children) -1
                 self.c_fitnesses =numpy.append(self.c_fitnesses, a)
                 self.c_fitnesses = numpy.append(self.c_fitnesses, b) #a before b(in order in which individuals were appended)!
 
@@ -307,21 +323,30 @@ class Population():
             else:
                 return False
         else:
+            """
             for i in self.individuals:
                 if i.fitness()[0]==9999:
                     i.show()
                     return True
+            """
+            if self.best_fitness == 9999:
+                assert self.individuals[self.bfi].fitness()[0] == 9999
+                self.individuals[self.bfi].show()
+                return True
+            else:
+                print(self.best_fitness)
         return False    
         
     def evolution(self,eons,mode):
         self.initialize()
         self.bsort()
         found = False
-        for i in range(eons):
+        for i in range(1,eons):
             if found == False:
-                self.battle()
+                if not self.just_initialized:
+                    self.battle("non_bias_random")
                 self.mating(mode)
-                found = self.check(mode)
+                found = self.check("speedy")
                 print(i)
                 #self.sorted = False not necessary, since sorted after check 
                 
@@ -329,16 +354,19 @@ class Population():
                 print(i)
                 print(len(self.individuals))
                 break
-            if i%10 == 0:
+            if (i)%10 == 0:
                 self.initialize()
                 self.sorted = False
                 self.roulette_ready = False
         print(len(self.individuals))
 
 def test(j,m,n,k,mode):
-    world = Population(2000,m,n,k)
+    world = Population(400,m,n,k)
     world.num_genes =j
     world.evolution(10000,mode)
         
+ 
+                
+            
+            
 
-    
